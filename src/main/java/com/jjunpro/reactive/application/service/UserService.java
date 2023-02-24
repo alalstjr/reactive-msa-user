94f5019fc2e702096c5bc3a683c7ec6031d601de
jjunpro <alalstjr@naver.com>
2023-02-25 00:51:49 +0900

    add : 사용자 서비스에 주석 추가

--- src/main/java/com/jjunpro/reactive/application/service/UserService.java
+++ src/main/java/com/jjunpro/reactive/application/service/UserService.java
@@ -15,6 +15,12 @@ import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+/**
+ * 사용자 서비스
+ *
+ * @author jjunpro
+ * @since 2023/02/25 AM 12:45
+ */
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -23,23 +29,45 @@ public class UserService {
     private final TeamRepository teamRepository;
     private final UserRepository userRepository;
 
+    /**
+     * 사용자 정보를 모두 탐색
+     * @return
+     */
     public Flux<GetUserDto> findAllUsers() {
-        return userRepository.findAll()
-                             .flatMap(user -> Flux.just(user.toGetUserDto()));
+        return this.userRepository
+                .findAll()
+                .flatMap(user -> Flux.just(user.toGetUserDto()));
     }
 
+    /**
+     * id 정보를 가지고 사용자 정보를 탐색
+     * @param userId
+     * @return
+     */
     public Mono<GetUserDto> findById(String userId) {
-        return userRepository.findById(userId)
-                             .map(User::toGetUserDto)
-                             .switchIfEmpty(Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "id doesn't exist")));
+        return this.userRepository
+                .findById(userId)
+                .map(User::toGetUserDto)
+                .switchIfEmpty(Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "id doesn't exist")));
     }
 
+    /**
+     * username 정보를 가지고 사용자 정보를 탐색
+     * @param username
+     * @return
+     */
     public Mono<GetUserDto> findByUsername(String username) {
-        return userRepository.findByUsername(username)
-                             .map(User::toGetUserDto)
-                             .switchIfEmpty(Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "username doesn't exist")));
+        return this.userRepository
+                .findByUsername(username)
+                .map(User::toGetUserDto)
+                .switchIfEmpty(Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "username doesn't exist")));
     }
 
+    /**
+     * 사용자 정보 저장
+     * @param createUserDtoMono
+     * @return
+     */
     public Mono<GetUserDto> addUser(Mono<CreateUserDto> createUserDtoMono) {
 
         return createUserDtoMono
@@ -73,19 +101,20 @@ public class UserService {
             });
     }
 
-    private Mono<GetUserDto> createUser(CreateUserDto createUserDto) {
-        var user = createUserDto.toUser();
-        return userRepository
-            .save(user)
-            .map(User::toGetUserDto);
-    }
-
+    /**
+     * 사용자 정보 삭제
+     * @param userId
+     * @return
+     */
     public Mono<GetUserDto> deleteUser(String userId) {
         return userRepository
             .findById(userId)
-            .flatMap(user -> userRepository.delete(userId)
-                                           .then(deleteMemberFromTeam(user)))
-            .switchIfEmpty(Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "cannot find user to delete")));
+            .flatMap(
+                user -> userRepository.delete(userId).then(deleteMemberFromTeam(user))
+            )
+            .switchIfEmpty(
+                Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "cannot find user to delete"))
+            );
     }
 
     private Mono<GetUserDto> deleteMemberFromTeam(User member) {
@@ -94,27 +123,52 @@ public class UserService {
         if (teamId != null) {
             return teamRepository
                 .findById(teamId)
-                .flatMap(team -> {
-                    TeamUtils.toMembers.apply(team).remove(member);
-                    return teamRepository.save(team)
-                                         .flatMap(t -> Mono.just(member.toGetUserDto()));
-                })
-                .switchIfEmpty(Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "cannot find user's team")));
+                .flatMap(
+                    team -> {
+                        TeamUtils.toMembers.apply(team).remove(member);
+                        return teamRepository.save(team).flatMap(t -> Mono.just(member.toGetUserDto()));
+                    }
+                )
+                .switchIfEmpty(
+                    Mono.error(new UserServiceException(HttpStatus.UNPROCESSABLE_ENTITY, "cannot find user's team"))
+                );
         }
         return Mono.just(member.toGetUserDto());
     }
 
-    public Mono<Boolean> checkIfUsernameExists(String username) {
+    /**
+     * username 중복으로 존재여부 유효성 검사
+     * @param username
+     * @return
+     */
+    private Mono<Boolean> checkIfUsernameExists(String username) {
         return userRepository
             .findByUsername(username)
             .map(user -> true) // username 이 이미 존재하는 경우
             .defaultIfEmpty(false); // username 이 존재하지 않는 경우
     }
 
-    public Mono<Boolean> checkIfNicknameExists(String nickname) {
+    /**
+     * nickname 중복으로 존재여부 유효성 검사
+     * @param nickname
+     * @return
+     */
+    private Mono<Boolean> checkIfNicknameExists(String nickname) {
         return userRepository
             .findByNickname(nickname)
             .map(user -> true) // nickname 가 이미 존재하는 경우
             .defaultIfEmpty(false); // nickname 가 존재하지 않는 경우
+    }
+
+    /**
+     * 사용자 정보를 생성합니다.
+     * @param createUserDto
+     * @return
+     */
+    private Mono<GetUserDto> createUser(CreateUserDto createUserDto) {
+        var user = createUserDto.toUser();
+        return userRepository
+            .save(user)
+            .map(User::toGetUserDto);
     }
 }
